30c60c8ed7ab266213f3510587050dec27f08e3a
Jono <30672425+cutted@users.noreply.github.com>
2018-11-25 18:52:24 +0000

    Update twitter_profile_lookup.py

--- twitter_profile_lookup.py
+++ twitter_profile_lookup.py
@@ -1,9 +1,13 @@
-
 import webbrowser
 
-print("This is a brief twitter profile viewer.")
-question = input("What profile would you like to visit? ")
-variable = 'https://twitter.com/intent/user?screen_name=' + question
+def body():
+  print("This is a brief twitter profile viewer.")
+  question = input("What profile would you like to visit? ")
+  variable = 'https://twitter.com/intent/user?screen_name=' + question
 
-print("Visiting", variable)
-webbrowser.open(variable)
+  print("Visiting", variable)
+  webbrowser.open(variable)
+  
+def main():
+  body()
+main()
