b85c6fdc1dbf70a282393129d45f0e60e83e84d2
Jono <30672425+cutted@users.noreply.github.com>
2018-11-25 19:10:44 +0000

    Create coinflips.py

---
+++ week_08/coinflips.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Practical Worksheet 8: Design and Simulation
+# coinflips.py
+#-------------------------------------------------------------------------------
+from random import random
+
+def getInputs():
+    # asks the user for their input of coin flips
+    amount = int(input("How many times would you like the coin to flip? " ))
+    simulateFlips(amount)
+
+def simulateFlips(amount):
+    # main bulk of program
+    heads = 0
+    tails = 0
+    for i in range(amount):
+        number = random() 
+        if number < 0.5:
+            heads = heads + 1 / amount
+        else:
+            tails = tails + 1 / amount
+    displayResults(heads, tails)
+
+def displayResults(heads, tails):
+    # this shows the proportion of the heads to tails ie how many heads to tails that were flipped
+    print("Heads", round(heads, 3), ":", "Tails", round(tails, 3))
+
+def main():
+    # executes the program
+    getInputs()
+main()
