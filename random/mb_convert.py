2e2c06690707ed632942635c6c64db3aab657da3
Jono <30672425+cutted@users.noreply.github.com>
2018-11-26 08:55:54 +0000

    update

--- random/mb_convert.py
+++ random/mb_convert.py
@@ -1,7 +1,8 @@
 def megabyteConvert():
   #this converts megabytes into bits, bytes, kilobytes, gigabytes and terrabytes
+  # written well before I knew how to actually do python!
 
-  megabyte = input("How many MB's do you have? ")
+  megabyte = float(input("How many MB's do you have? "))
   terabyte = float(megabyte) / 1048576
   gigabyte = float(megabyte) / 1024
   kilobyte = float(megabyte) * 1024
@@ -16,5 +17,5 @@ def megabyteConvert():
   print("\t" + str(gigabyte), "GB")
   print("\t" + str(terabyte), "TB")
 def main():
-  def megabyteConvert()
+  megabyteConvert()
 main()
